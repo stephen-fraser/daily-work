c7159938e66143e9863913734b88010f1b74b601
stephen-fraser <steve.james.fraser@gmail.com>
2023-12-15 11:50:40 -0800

    initial add of findFriend with seed data

---
+++ findFriend/findFriend.js
@@ -0,0 +1,33 @@
+// EXAMPLE DATA BELOW
+const contacts = [
+  {
+    name: "Laurel",
+    phone: "123 456 7890",
+    email: "laurel@comics.com",
+    friends: ["Hardy", "Abbott", "Costello"],
+  },
+  {
+    name: "Hardy",
+    phone: "321 654 0987",
+    email: "hardy@hardyharhar.com",
+    friends: ["Laurel", "Buster"],
+  },
+  {
+    name: "Buster",
+    phone: "987 654 3210",
+    email: "buster@keaton.ca",
+    friends: ["Hardy"],
+  },
+  {
+    name: "Abbott",
+    phone: "888 123 4567",
+    email: "abbott@whosonfirst.co",
+    friends: ["Costello", "Laurel"],
+  },
+  {
+    name: "Costello",
+    phone: "767 676 7676",
+    email: "costello@imonfirst.co",
+    friends: ["Abbott", "Laurel"],
+  },
+];
